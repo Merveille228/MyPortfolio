1cb850c96166094cbb51655fc0acc7c0a9aea988
merveille228 <merveilleamevigbe2@gmail.com>
2025-11-12 14:39:15 +0000

    commit initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center">
+        <p className="text-center text-6xl font-bold text-white">
+          Hello Portfolio ✨
+        </p>
+      </div>
+    </>
+  )
+}
+
+export default App
